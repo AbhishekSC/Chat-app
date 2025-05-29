f31ab5164e96608d0d7cff4b65a16aefac7e0f22
AbhishekSC <rams31824@gmail.com>
2025-05-29 22:52:13 +0530

    fixing cors error- 2

--- backend/src/index.js
+++ backend/src/index.js
@@ -54,9 +54,9 @@ app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 // ***To enable CORS (Cross-Origin Resource Sharing)***
 const corsOptions = {
   origin:
-    import.meta.env.MODE === "development"
-      ? "http://localhost:5173"
-      : "https://secretwire.onrender.com", // <-- your frontend domain
+    process.env.NODE_ENV === "production"
+      ? "https://secretwire.onrender.com" // <-- your frontend domain
+      : "http://localhost:5173",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: [
